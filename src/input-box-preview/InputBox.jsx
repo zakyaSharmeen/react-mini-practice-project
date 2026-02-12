3ba233d58aec063ad7fae38e9b72ed83c5f20160
zakyaSharmeen <zakyasharmeen@gmail.com>
2026-02-12 23:13:08 +0530

    added

--- src/input-box-preview/InputBox.jsx
+++ src/input-box-preview/InputBox.jsx
@@ -1,12 +1,26 @@
-import React from "react";
+// import React from "react";
 
-function InputBox() {
+// function InputBox() {
+//   return (
+//     <div>
+//       <h1>hii zakya u have to do it</h1>
+//       <p>just focus on ur self and evrything will be set</p>
+//     </div>
+//   );
+// }
+
+// export default InputBox;
+import { useState } from "react";
+
+function App() {
+  const [text, setText] = useState("");
+
   return (
     <div>
-      <h1>hii zakya u have to do it</h1>
-      <p>just focus on ur self and evrything will be set</p>
+      <input type="text" onChange={(e) => setText(e.target.value)} />
+      <p>Preview: {text}</p>
     </div>
   );
 }
 
-export default InputBox;
+export default App;
